3f3e8d2761c1139588c8e2a36e5dc86e96715a94
Tom Mikota <tmikota@gmail.com>
2021-11-28 21:50:47 -0500

    added files for mdl prepublish.

---
+++ cookbook/maya/pre_publish/mdl/CleanTt.py
@@ -0,0 +1,24 @@
+from cgl.plugins.preflight.preflight_check import PreflightCheck
+from cgl.plugins.maya import alchemy as alc
+
+
+class CleanTt(PreflightCheck):
+
+    def getName(self):
+        pass
+
+    def run(self):
+        """
+        script to be executed when the preflight is run.
+
+        If the preflight is successful:
+        self.pass_check('Message about a passed Check')
+
+        if the preflight fails:
+        self.fail_check('Message about a failed check')
+        :return:
+        """
+        print('PreflightTemplate')
+        alc.clean_turntable()
+        self.pass_check('Check Passed')
+        # self.fail_check('Check Failed')
