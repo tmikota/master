aca6958459bb78efe3d2ac6e77a953d7754a2e81
tmikota@gmail.com <321Chevon>
2021-12-06 12:43:54 -0500

    updates

--- cookbook/maya/pre_publish/mdl/Publish.py
+++ cookbook/maya/pre_publish/mdl/Publish.py
@@ -1,6 +1,4 @@
 from cgl.plugins.preflight.preflight_check import PreflightCheck
-# there is typically a alchemy.py, and utils.py file in the plugins directory.
-# look here for pre-built, useful functions
 from cgl.plugins.maya import alchemy as alc
 
 
@@ -10,16 +8,6 @@ class Publish(PreflightCheck):
         pass
 
     def run(self):
-        """
-        script to be executed when the preflight is run.
 
-        If the preflight is successful:
-        self.pass_check('Message about a passed Check')
-
-        if the preflight fails:
-        self.fail_check('Message about a failed check')
-        :return:
-        """
-		alc.scene_object().publish()
+        alc.scene_object().publish()
         self.pass_check('Check Passed')
-		# close the window.
