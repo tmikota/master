b682f23e51843650dc93c68d08418946ccdb9939
tmikota@gmail.com <321Chevon>
2021-12-17 11:32:31 -0500

    Added the Rig Pre-Publish stuff.

--- cookbook/unreal/menus/Capernaum/PlaceMigratedAssets.py
+++ cookbook/unreal/menus/Capernaum/PlaceMigratedAssets.py
@@ -2,3 +2,6 @@ from cgl.plugins.unreal_engine.utils import place_migrated_assets
 
 def run():
     place_migrated_assets()
+
+if __name__ == '__main__':
+    run()
--- cookbook/unreal/menus/PreProduction/MigrateSelectedAsset.py
+++ cookbook/unreal/menus/PreProduction/MigrateSelectedAsset.py
@@ -3,3 +3,6 @@ from cgl.plugins.unreal_engine.utils import migrate_selected_asset
 def run():
     dest_uproject_path = r'C:\Users\VP01\Documents\Unreal Projects\MigrationTest\MigrationTest.uproject'
     migrate_selected_asset(destination_uproject_path=dest_uproject_path)
+
+if __name__ == '__main__':
+    run()
