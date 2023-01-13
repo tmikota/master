489ea362a8993c4eb6394d6129270a3d554b3f0b
Tom Mikota <tmikota@gmail.com>
2023-01-13 17:48:46 -0500

    Adjusting Globals for Premise

---
+++ cookbook/maya/pre_publish/mdl/CreateMsd.py
@@ -0,0 +1,19 @@
+from cgl.plugins.preflight.preflight_check import PreflightCheck
+import cgl.core.msd as msd
+import cgl.plugins.maya.alchemy_new as alc
+
+
+class CreateMsd(PreflightCheck):
+
+    def getName(self):
+        pass
+
+    def run(self):
+        """
+        writes out an msd file for the current scene
+        """
+        # we could use something like this if we actually want to use dependencies on these.
+        # dependency_dict = {"mdl": [],
+        #                    "anim": ['cam']}
+        msd_obj = msd.MSD(path_object=alc.Scene().path_object, dcc="maya")
+        msd_obj.write_msd()
---
+++ cookbook/maya/shelves/mdl/ModelRenamer.py
@@ -0,0 +1,10 @@
+import cgl.plugins.maya.tools.renamer.ui as ui
+
+
+def run():
+    """
+    Launches the Model Renamer
+    """
+    this = ui.ModelRenamer()
+    this.exec_()
+
